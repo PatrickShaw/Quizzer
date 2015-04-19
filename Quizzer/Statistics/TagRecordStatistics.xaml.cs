c440347c96ba3697baac3645148318585db2d590
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2015-04-20 05:01:24 +1000

    Quizzer 1.0.0a
    Changelog =======
    Statistics Page
    Lots and lots of code refactoring. There is still more to go. This will
    increase the rate of development.
    Graphs now have borders
    Slight positioning adjustments.
    
    Removed Bugs
    Program would crash if you add an image path without a file extension.
    Tab naming scheme has been corrected.
    The 'Exam theme' appears to be working again.
    A random tag will no longer be added if you did not add a tag to the
    question.
    
    ModuleTabControl
    Clicking on 'Question Manager' will no longer cause lag if there is
    already a Question Manager present.
    Clicking on 'Question Manager' if there is already a Question Manager
    open will cause Quizzer to focus on the Question Manager that is already
    open.

---
+++ Quizzer/Statistics/TagRecordStatistics.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quizzer
+{
+    /// <summary>
+    /// Interaction logic for TagRecordStatistics.xaml
+    /// </summary>
+    public partial class TagRecordStatistics : UserControl
+    {
+        string _recordName = "";
+        public string RecordsName
+        {
+            set { _recordName = value;
+            txbBestRecordTitle.Text = "Best " + _recordName;
+            txbWorstRecordTitle.Text = "Worst " + _recordName;
+            }
+            get { return _recordName; }
+        }
+        List<Tag> _tags = new List<Tag>();
+        public List<Tag> Tags
+        { 
+            set 
+            {
+                _tags = value;
+                GenerateRecord();
+            }
+            get { return _tags; }
+        }
+        public void GenerateRecord()
+        {
+            lstRecordVSMarks.Items.Clear();
+            
+                string bestSubject = "Requires more questinos to be answered";
+                double bestSubjectPoints = 2;
+                string worstSubject = "Requires more questionis to be answered";
+                double worstSubjectPoints = -1;
+                _tags = _tags.OrderBy(x => x.WrongPercentage).ThenByDescending(y => (y.TimesRight - y.TimesWrong)).ThenBy(z => z.TimesRight).ToList();
+                foreach (Tag SST in _tags)
+                { 
+                    ListBoxItem lstSST = new ListBoxItem();
+                    TextBlock txbT = new TextBlock();
+                    txbT.FontSize = 14;
+                    txbT.TextAlignment = TextAlignment.Right;
+                    Run subject = new Run(SST.Name + " - ");
+                    Run c = new Run((SST.TimesAnswered - SST.TimesWrong).ToString());
+                    Run w = new Run(SST.TimesWrong.ToString());
+                    Run slash = new Run("\\");
+                    c.Foreground = new SolidColorBrush(Color.FromRgb(75, 255, 75));
+                    w.Foreground = new SolidColorBrush(Color.FromRgb(255, 75, 75));
+                    txbT.Inlines.Add(subject);
+                    txbT.Inlines.Add(c);
+                    txbT.Inlines.Add(slash);
+                    txbT.Inlines.Add(w);
+                    lstSST.Content = txbT;
+                    lstRecordVSMarks.Items.Add(lstSST);
+                    double wrongRatio = Convert.ToDouble(SST.TimesWrong) / Convert.ToDouble(SST.TimesAnswered);
+                    if (SST.TimesAnswered >= 4)
+                    {
+                        if (wrongRatio >= worstSubjectPoints)
+                        {
+                            worstSubject = SST.Name;
+                            worstSubjectPoints = wrongRatio;
+                        }
+                        if (wrongRatio <= bestSubjectPoints)
+                        {
+                            bestSubjectPoints = wrongRatio;
+                            bestSubject = SST.Name;
+                        }
+                    }
+                    txbBestRecord.Text = bestSubject;
+                    txbWorstRecord.Text = worstSubject;
+                }
+        }
+        public TagRecordStatistics()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Shaw Tab/ModuleTabControl.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+namespace Shaw_Tab
+{
+    /// <summary>
+    /// Interaction logic for ModuleTabControl.xaml
+    /// </summary>
+    public partial class ModuleTabControl : TabControl
+    {
+        public bool tracked = true;
+        public Point startPoint;
+
+        public void AddTab(object control, string title, bool duplicatesAllowed = true)
+        {
+            int copies = ModuleCache.NoOpened(control.GetType());
+           // MessageBox.Show(copies.ToString());
+            if (copies >= 1 && !duplicatesAllowed)
+            {
+                ModuleCache.FocusOnClosestTab(control.GetType());
+                return;
+            }
+
+            int highest = int.MinValue;
+            for(int i = 0; i < ModuleCache.draggedTabs.Count;i++)
+            {
+                if(ModuleCache.draggedTabs[i].Content.GetType() == control.GetType())
+                {
+                    if(highest < ModuleCache.draggedTabs[i].copy)
+                    {
+                        highest = ModuleCache.draggedTabs[i].copy;
+                    }
+                }
+            }
+            int copyOfTheNewModule = -1;
+            if(highest == int.MinValue)
+            {
+                copyOfTheNewModule = 0;
+            }
+            else
+            {
+                for(int i = 0 ; i <= (highest+1);i++)
+                {
+                    bool found = false;
+                    for(int q= 0 ; q < ModuleCache.draggedTabs.Count;q++)
+                    {
+                        if(ModuleCache.draggedTabs[q].Content.GetType() == control.GetType())
+                        if (ModuleCache.draggedTabs[q].copy == i) { found = true; break; }
+                    }
+                    if (!found) { copyOfTheNewModule = i; break; }
+                }
+            }
+            ModuleTabItem tabItemT = new ModuleTabItem(title, copyOfTheNewModule); 
+                ModuleCache.draggedTabs.Add(tabItemT);
+            tabItemT.Content = control;
+            Items.Add(tabItemT);
+            SelectedIndex = Items.Count - 1;
+            SelectedItem = tabItemT;
+            tabItemT.IsSelected = true;
+            CheckItems();
+        }
+        public void AddTab(object control, string title, int copyNo, bool duplicatesAllowed = true)
+        {
+            ModuleTabItem tabItemT = new ModuleTabItem(title, copyNo);
+            tabItemT.Content = control;
+            Items.Add(tabItemT);
+            SelectedIndex = Items.Count - 1;
+            SelectedItem = tabItemT;
+            tabItemT.IsSelected = true;
+            CheckItems();
+            //((UserControl)tabItemT.Content).Width = Cache.sWidth;
+            //((UserControl)tabItemT.Content).Height = Cache.sHeight;
+        }
+        public void CheckItems()
+        { 
+            /*
+            if (!(this.Parent is ModuleTabControl)) { return; }
+            if (Items.Count <= 1)
+            {
+                foreach (TabItem tab in Items)
+                {
+                    tab.Visibility = System.Windows.Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                foreach (TabItem tab in Items)
+                {
+                    tab.Visibility = System.Windows.Visibility.Visible;
+                }
+            }*/
+        }
+
+        public void TabItem_Unloaded(object sender, RoutedEventArgs e)
+        {
+            ModuleCache.draggedTabs.Remove((ModuleTabItem)sender);
+            if (tracked)
+            {
+                ModuleCache.userControlsOpen.Remove(((TabItem)sender).Content.GetType());
+            }
+            Items.Remove(sender);
+            CheckItems();
+            if (Items.Count == 0)
+            {
+                UndockedModule undockedModule = Parent as UndockedModule;
+                if (undockedModule != null)
+                {
+                    undockedModule.Close();
+                }
+            }
+        }
+
+        public ModuleTabControl(bool trackedT)
+        {
+            tracked = trackedT;
+            InitializeComponent();
+        }
+        public ModuleTabControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                case Key.Right:
+                case Key.Up:
+                case Key.Down:
+                    e.Handled = true;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+
+        private void TabControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //Debug.WriteLine(Cache.draggedTabs.Count);
+            //for (int i = Cache.draggedTabs.Count -1; 0<=i ;i--)
+            //{
+            //Begin:
+            //    if (i<0) { break; }
+            //    for (int o = Items.Count-1; 0<=o;o--)
+            //    {
+            //        if (Cache.draggedTabs[i] == Items[o]) { i--; goto Begin; }
+            //    }
+            //Cache.draggedTabs[i].Detach();
+            //Items.Add(Cache.draggedTabs[i]);
+            //}
+            //CheckItems();
+        } 
+        private void TabControl_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void TabControl_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+        }
+
+        private void TabControl_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TabControl_Drop(object sender, DragEventArgs e)
+        {
+            ModuleTabItem tabItem = e.Data.GetData(typeof(ModuleTabItem)) as ModuleTabItem;
+            ModuleTabControl oldControl = tabItem.Parent as ModuleTabControl;
+            //if (e.Source.Equals(this)) { return; }
+            oldControl.Items.Remove(tabItem);
+            Items.Add(tabItem);
+        }
+
+        private void TabControl_PreviewDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                ModuleTabItem tabItem = e.Data.GetData(typeof(ModuleTabItem)) as ModuleTabItem;
+                ModuleTabControl oldControl = tabItem.Parent as ModuleTabControl;
+                if (oldControl.Equals(this))
+                {
+                    e.Handled = true;
+                }
+            }
+            catch
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
